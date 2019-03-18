fb233991d9dc2c4bc66113fb29b429e15304bfcc
tommy-jerry <tom_jerry@163.com>
2019-03-18 21:36:38 +0800

    add batch script
    Signed-off-by: tommy-jerry <tom_jerry@163.com>

--- run_batch_2way.sh
+++ run_batch_2way.sh
@@ -1,18 +1,13 @@
 #! /bin/sh -
 seed=$1
-for file in `ls ssbse09benchmarks/*.constraints | grep -v -E '_[0-9]+\.'`
-do
-    for ((tabu_size=700; tabu_size<=2000; tabu_size=tabu_size+300))
-    do
-        for ((random_size=1; random_size<=10; random_size=random_size+2))
-        do
-            base_param=$(echo $file|cut -d "." -f 1)
-            name=$(echo $base_param|cut -d '/' -f 2) 
-            echo ./TCA ./${base_param}_2way.model ./$file 1000 $seed $tabu_size $random_size
-            #./TCA ./${base_param}_2way.model ./$file 1000 $seed $tabu_size $random_size > error.${name}_${seed}_${tabu_size}_${random_size}_2way 2>&1 
-
-        done
-    done
+tabu_size=1000
+random_size=5
+for file in `ls ssbse09benchmarks/*.constraints `
+do  
+    base_param=$(echo $file|cut -d "." -f 1)
+    name=$(echo $base_param|cut -d '/' -f 2) 
+    echo ./TCA ./${base_param}_2way.model ./$file 1000 $seed $tabu_size $random_size
+    #./TCA ./${base_param}_2way.model ./$file 1000 $seed $tabu_size $random_size > error.${name}_${seed}_${tabu_size}_${random_size}_2way 2>&1 
 
 done
 
--- run_batch_3way_grid.sh
+++ run_batch_3way_grid.sh
@@ -10,15 +10,18 @@
 #done
 
 seed=$1
-tabu_size=1000
-random_size=5
 
-
-for file in `ls ssbse09benchmarks_3way/*.constraints `
+for file in `ls ssbse09benchmarks_3way/*.constraints | grep -v -E '_[0-9]+\.'`
 do
-    base_param=$(echo $file|cut -d "." -f 1)
-    name=$(echo $base_param|cut -d '/' -f 2) 
-    echo ./TCA ./${base_param}_3way.model ./$file 1000 $seed $tabu_size $random_size
+    for ((tabu_size=700; tabu_size<=2000; tabu_size=tabu_size+300))
+    do
+        for ((random_size=1; random_size<=10; random_size=random_size+2))
+        do
+            base_param=$(echo $file|cut -d "." -f 1)
+            name=$(echo $base_param|cut -d '/' -f 2) 
+            echo ./TCA ./${base_param}_3way.model ./$file 1000 $seed $tabu_size $random_size
             #./TCA ./${base_param}_3way.model ./$file 1000 $seed $tabu_size $random_size> error.${name}_${seed}_${tabu_size}_${random_size}_3way 2>&1
+        done
+    done
 
 done
